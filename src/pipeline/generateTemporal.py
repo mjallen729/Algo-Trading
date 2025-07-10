d094c282f9b0c58beae308b2d94c4752a72d0e31
Matt <mjallen729@gmail.com>
2025-07-09 20:43:09 -0400

    new preprocessing

--- src/pipeline/generateTemporal.py
+++ src/pipeline/generateTemporal.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pytorch_forecasting import GroupNormalizer, TimeSeriesDataSet
 
 """
+INBOUND DATA (crypro_processed.csv)
   0   time_idx     1120061 non-null  int64
   1   symbol       1120061 non-null  object
   2   open         1120061 non-null  float64
@@ -17,21 +18,30 @@ from pytorch_forecasting import GroupNormalizer, TimeSeriesDataSet
   12  month        1120061 non-null  int32  
   13  day          1120061 non-null  int32
 """
-def preprocessing(df: pd.DataFrame) -> pd.DataFrame:
+
+
+def _preprocessing(df: pd.DataFrame) -> pd.DataFrame:
+  # Mark all time series categories
   df['hour'] = df['hour'].astype(str).astype('category')
   df['weekday'] = df['weekday'].astype('category')
   df['month'] = df['month'].astype(str).astype('category')
   df['day'] = df['day'].astype(str).astype('category')
+
+  # Convert to floats for normalizers
+  df['volume usdt'] = df['volume usdt'].astype("float64")
+  df['tradecount'] = df['tradecount'].astype("float64")
+  df['year'] = df['year'].astype("float64")
+
   return df
 
-def generateTemporal(df: pd.DataFrame):
-  df = preprocessing(df)
+
+def generateTemporal(df: pd.DataFrame) -> TimeSeriesDataSet:
+  df = _preprocessing(df)
 
   dataset = TimeSeriesDataSet(
     data=df,
     time_idx="time_idx",
     target="close",
-    # each token/symbol is its own series
     group_ids=["symbol"],
     min_encoder_length=72,
     max_encoder_length=72,
@@ -40,23 +50,24 @@ def generateTemporal(df: pd.DataFrame):
     static_categoricals=["token"],
     time_varying_known_categoricals=["hour", "weekday", "month", "day"],
     time_varying_known_reals=["time_idx", "year"],
-    time_varying_unknown_reals=["open", "high", "low", "close", "volume usdt", "tradecount"],
-    target_normalizer= GroupNormalizer(
-      groups=["symbol"], transformation="softplus"
-    ).fit_transform(df['close'], df),
-    scalers= {
-      "open": GroupNormalizer(groups=['symbol'], transformation='softplus'),
-      "high": GroupNormalizer(groups=['symbol'], transformation='softplus'),
-      "low": GroupNormalizer(groups=['symbol'], transformation='softplus'),
-      "close": GroupNormalizer(groups=['symbol'], transformation='softplus'),
-      "volume usdt": GroupNormalizer(groups=['symbol'], transformation='softplus'),
-      "tradecount": GroupNormalizer(groups=['symbol'], transformation='softplus'),
-      "year": GroupNormalizer(groups=['symbol'], transformation='softplus')
+    time_varying_unknown_reals=["open", "high",
+                                "low", "close", "volume usdt", "tradecount"],
+    target_normalizer=GroupNormalizer(
+      groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
+    scalers={
+      "open": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
+      "high": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
+      "low": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
+      "volume usdt": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
+      "tradecount": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
+      "year": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]])
     }
   )
 
   return dataset
 
+
 if __name__ == '__main__':
-  p = preprocessing(pd.read_csv('~/Documents/Algo-Trading/data/crypto_processed.csv'))
+  p = _preprocessing(pd.read_csv(
+    '~/Documents/Algo-Trading/data/crypto_processed.csv'))
   tp = generateTemporal(p)
