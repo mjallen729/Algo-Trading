16675fdbc5f1a52bf2dbd7aa0b764e83dc9da98d
Matt <mjallen729@gmail.com>
2025-07-10 00:17:41 -0400

    update

--- src/pipeline/generateTemporal.py
+++ src/pipeline/generateTemporal.py
@@ -3,20 +3,19 @@ from pytorch_forecasting import GroupNormalizer, TimeSeriesDataSet
 
 """
 INBOUND DATA (crypto_processed.csv)
-  0   time_idx     1120061 non-null  int64
-  1   symbol       1120061 non-null  object
+  0   time_idx     1120061 non-null  int64  
+  1   symbol       1120061 non-null  object 
   2   open         1120061 non-null  float64
   3   high         1120061 non-null  float64
   4   low          1120061 non-null  float64
   5   close        1120061 non-null  float64
-  6   volume usdt  1120061 non-null  int64
-  7   tradecount   1120061 non-null  int64
-  8   token        1120061 non-null  object
-  9   hour         1120061 non-null  int64
-  10  weekday      1120061 non-null  object 
-  11  year         1120061 non-null  int32  
-  12  month        1120061 non-null  int32  
-  13  day          1120061 non-null  int32
+  6   volume usdt  1120061 non-null  int64  
+  7   token        1120061 non-null  object 
+  8   hour         1120061 non-null  int64  
+  9   weekday      1120061 non-null  object 
+  10  year         1120061 non-null  int32  
+  11  month        1120061 non-null  int32  
+  12  day          1120061 non-null  int32 
 """
 
 
@@ -29,8 +28,6 @@ def _preprocessing(df: pd.DataFrame) -> pd.DataFrame:
 
   # Convert to floats for normalizers
   df['volume usdt'] = df['volume usdt'].astype("float64")
-  df['tradecount'] = df['tradecount'].astype("float64")
-  df['year'] = df['year'].astype("float64")
 
   return df
 
@@ -50,17 +47,14 @@ def generateTemporal(df: pd.DataFrame) -> TimeSeriesDataSet:
     static_categoricals=["token"],
     time_varying_known_categoricals=["hour", "weekday", "month", "day"],
     time_varying_known_reals=["time_idx", "year"],
-    time_varying_unknown_reals=["open", "high",
-                                "low", "close", "volume usdt", "tradecount"],
+    time_varying_unknown_reals=["open", "high", "low", "close", "volume usdt"],
     target_normalizer=GroupNormalizer(
       groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
     scalers={
-      "open": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
-      "high": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
-      "low": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
-      "volume usdt": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
-      "tradecount": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]]),
-      "year": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["close"], X=df[["symbol"]])
+      "open": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["open"], X=df[["symbol"]]),
+      "high": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["high"], X=df[["symbol"]]),
+      "low": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["low"], X=df[["symbol"]]),
+      "volume usdt": GroupNormalizer(groups=['symbol'], transformation='softplus').fit(y=df["volume usdt"], X=df[["symbol"]]),
     }
   )
 
